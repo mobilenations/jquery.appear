634705b8766f60e216c44d25569a60dab8ae99f9
Tom Kaminski <ttkaminski@gmail.com>
2020-01-30 12:36:34 -0600

    Add once option.  Add marginX and marginY options. Improve the bounds checking

--- index.js
+++ index.js
@@ -5,11 +5,12 @@
   var checkLock = false;
   var defaults = {
     interval: 250,
-    force_process: false
+    force_process: false,
+    once: false,
+    marginX: 0,
+    marginY: 0
   };
   var $window = $(window);
-
-  var $priorAppeared = [];
 
   function isAppeared() {
     return $(this).is(':appeared');
@@ -19,30 +20,47 @@
     return !$(this).data('_appear_triggered');
   }
 
+  function isOnce() {
+    return $(this).data('_appear_options').once;
+  }
+
   function process() {
     checkLock = false;
 
-    for (var index = 0, selectorsLength = selectors.length; index < selectorsLength; index++) {
-      var $appeared = $(selectors[index]).filter(isAppeared);
+    for (var index = 0; index < selectors.length; index++) {
+      var sel = selectors[index];
+      var $appeared = $(sel.selector).filter(isAppeared);
 
       $appeared
         .filter(isNotTriggered)
         .data('_appear_triggered', true)
         .trigger('appear', [$appeared]);
 
-      if ($priorAppeared[index]) {
-        var $disappeared = $priorAppeared[index].not($appeared);
+      var $once = $appeared.filter(isOnce);
+      if ($once.length) {
+        $appeared = $appeared.not($once);
+        sel.selector = sel.selector.not($once);
+        if (!sel.selector.length) {
+          selectors.splice(index--,1);
+          continue;
+        }
+      }
+
+      if (sel.priorAppeared && sel.priorAppeared.length) {
+        var $disappeared = sel.priorAppeared.not($appeared);
         $disappeared
           .data('_appear_triggered', false)
           .trigger('disappear', [$disappeared]);
       }
-      $priorAppeared[index] = $appeared;
+      sel.priorAppeared = $appeared;
     }
   }
 
   function addSelector(selector) {
-    selectors.push(selector);
-    $priorAppeared.push();
+    selectors.push({
+      selector:selector,
+      priorAppeared: null
+    });
   }
 
   // ":appeared" custom filter
@@ -56,14 +74,32 @@
 
       var windowLeft = $window.scrollLeft();
       var windowTop = $window.scrollTop();
+      var windowRight = windowLeft + $window.width();
+      var windowBottom = windowTop + $window.height();
+
       var offset = $element.offset();
       var left = offset.left;
       var top = offset.top;
+      var right = offset.left + $element.width();
+      var bottom = offset.top + $element.height();
 
-      if (top + $element.height() >= windowTop &&
-          top - ($element.data('appear-top-offset') || 0) <= windowTop + $window.height() &&
-          left + $element.width() >= windowLeft &&
-          left - ($element.data('appear-left-offset') || 0) <= windowLeft + $window.width()) {
+      // Adjust the virtual size of element based on the margins (with appropriate overrides)
+      var options = $element.data('_appear_options');
+      var marginX = options.marginX;
+      if ((v = $element.data('appear-margin-x')) != undefined) marginX = v;
+      else if ((v = $element.data('appear-left-offset')) != undefined) marginX = v;
+
+      var marginY = options.marginY;
+      if ((v = $element.data('appear-margin-y')) != undefined) marginY = v;
+      else if ((v = $element.data('appear-top-offset')) != undefined) marginY = v;
+
+      left -= marginX;
+      right += marginX;
+      top -= marginY;
+      bottom += marginY;
+
+      if (bottom >= windowTop && top <= windowBottom &&
+          right >= windowLeft && left <= windowRight) {
         return true;
       }
       return false;
@@ -72,7 +108,7 @@
 
   $.fn.extend({
     // watching for element's appearance in browser viewport
-    appear: function (selector, options) {
+    appear: function (options) {
       $.appear(this, options);
       return this;
     }
@@ -81,6 +117,8 @@
   $.extend({
     appear: function (selector, options) {
       var opts = $.extend({}, defaults, options || {});
+      selector = $(selector); // Ensure it's a jQuery object
+      selector.data('_appear_options',opts);
 
       if (!checkBinded) {
         var onCheck = function () {
